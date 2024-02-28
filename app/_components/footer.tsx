c51641e9a5a4b3d613b997c2da8aeafc8b492d0a
Lucas Silva <lucas.silva182@outlook.com>
2024-02-27 21:12:41 -0300

    feat: add footer component

---
+++ app/_components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="w-full bg-secondary py-6 px-5 mt-[4.2rem]">
+      <p className="text=grey-400 text-xs font-bold opacity-75">
+        © 2023 Copyright FSW Barber
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
