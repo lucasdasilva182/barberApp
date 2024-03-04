188b1942f2172278f0e4bd0572d56c064a0adebe
Lucas Silva <lucas.silva182@outlook.com>
2024-03-04 15:27:20 -0300

    feat: add ServiceItem component

--- prisma/migrations/20240228110446_add_image_url_service/migration.sql
+++ prisma/migrations/20240228110446_add_image_url_service/migration.sql
@@ -1,8 +1,8 @@
 /*
   Warnings:
 
-  - Added the required column `imageURL` to the `Service` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `imageUrl` to the `Service` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
-ALTER TABLE "Service" ADD COLUMN     "imageURL" TEXT NOT NULL;
+ALTER TABLE "Service" ADD COLUMN     "imageUrl" TEXT NOT NULL;
