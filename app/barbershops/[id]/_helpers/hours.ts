40e7d407fb0c0cb7e00e9ff518df506279bc206e
Lucas Silva <lucas.silva182@outlook.com>
2024-04-24 14:55:59 -0300

    feat: ensure user can search barbershops

--- app/barbershops/[id]/_helpers/hours.ts
+++ app/barbershops/[id]/_helpers/hours.ts
@@ -12,7 +12,7 @@ const now = new Date();
 export function generateDayTimeList(date: Date): string[] {
   const startTime = isFuture(date)
     ? setMinutes(setHours(date, 9), 0)
-    : setMinutes(setHours(date, getHours(now) > 9 ? getHours(now) + 1 : 9), 0);
+    : setMinutes(setHours(date, getHours(now) > 8 ? getHours(now) + 1 : 9), 0);
   const endTime = setMinutes(setHours(date, 21), 0); // Set end time to 21:00
   const interval = 45; // interval in minutes
   const timeList: string[] = [];
