1ba8942ef347be0ffbdfc6fab7665ee236edee50
Lucas Silva <lucas.silva182@outlook.com>
2024-04-24 12:29:50 -0300

    fix: ensure first available day to book a service is tomorrow

--- app/barbershops/[id]/_helpers/hours.ts
+++ app/barbershops/[id]/_helpers/hours.ts
@@ -3,10 +3,16 @@ import {
   setMinutes,
   format,
   addMinutes,
+  isFuture,
+  getHours,
 } from 'date-fns';
 
+const now = new Date();
+
 export function generateDayTimeList(date: Date): string[] {
-  const startTime = setMinutes(setHours(date, 9), 0); // Set start time to 09:00
+  const startTime = isFuture(date)
+    ? setMinutes(setHours(date, 9), 0)
+    : setMinutes(setHours(date, getHours(now) > 9 ? getHours(now) + 1 : 9), 0); // Set start time to 09:00
   const endTime = setMinutes(setHours(date, 21), 0); // Set end time to 21:00
   const interval = 45; // interval in minutes
   const timeList: string[] = [];
