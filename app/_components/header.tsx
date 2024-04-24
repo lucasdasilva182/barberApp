ea1f8998f0fa9d301002baea85b5ec4d5d1f97c8
Lucas Silva <lucas.silva182@outlook.com>
2024-04-24 17:16:25 -0300

    feat: ensure semantic tags are used on footer and header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,40 +4,33 @@ import Image from 'next/image';
 import { Card, CardContent } from './ui/card';
 import { Button } from './ui/button';
 import { MenuIcon } from 'lucide-react';
-import {
-  Sheet,
-  SheetContent,
-  SheetTrigger,
-} from './ui/sheet';
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
 import SideMenu from './side-menu';
 import Link from 'next/link';
 
 const Header = () => {
   return (
-    <Card>
-      <CardContent className="p-5 justify-between flex flex-row items-center">
-        <Link href={'/'}>
-          <Image
-            src="/logo.png"
-            alt="Logo"
-            height={22}
-            width={120}
-          />
-        </Link>
+    <header>
+      <Card>
+        <CardContent className="p-5 justify-between flex flex-row items-center">
+          <Link href={'/'}>
+            <Image src="/logo.png" alt="Logo" height={22} width={120} />
+          </Link>
 
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline" size="icon">
-              <MenuIcon />
-            </Button>
-          </SheetTrigger>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon">
+                <MenuIcon />
+              </Button>
+            </SheetTrigger>
 
-          <SheetContent className="p-0">
-            <SideMenu />
-          </SheetContent>
-        </Sheet>
-      </CardContent>
-    </Card>
+            <SheetContent className="p-0">
+              <SideMenu />
+            </SheetContent>
+          </Sheet>
+        </CardContent>
+      </Card>
+    </header>
   );
 };
 
