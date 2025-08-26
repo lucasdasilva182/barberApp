87b6ecd29b657f7aa7aa6f877cd47a7d0e12936e
Lucas Silva <lucas.silva182@outlook.com>
2025-08-26 15:29:50 -0300

    style: changing badge global style

--- app/_components/ui/badge.tsx
+++ app/_components/ui/badge.tsx
@@ -1,20 +1,16 @@
 import * as React from 'react';
-import {
-  cva,
-  type VariantProps,
-} from 'class-variance-authority';
+import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/app/_lib/utils';
 
 const badgeVariants = cva(
-  'inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  'inline-flex items-center rounded-full border px-2.5 py-0.5 text-[11px] font-semibold leading-3 transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
     variants: {
       variant: {
-        default:
-          'border-transparent bg-[#221C3D] text-primary hover:bg-[#221C3D]',
-        secondary:
-          'border-transparent bg-secondary text-gray-400 hover:bg-secondary/80',
+        default: 'border-transparent bg-primary/50 text-primary-foreground',
+        secondary: 'border-transparent bg-secondary text-gray-400 hover:bg-secondary/80',
+        success: 'border-transparent bg-green-100/90 text-green-900',
         destructive:
           'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
         outline: 'text-foreground',
@@ -30,17 +26,8 @@ export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof badgeVariants> {}
 
-function Badge({
-  className,
-  variant,
-  ...props
-}: BadgeProps) {
-  return (
-    <div
-      className={cn(badgeVariants({ variant }), className)}
-      {...props}
-    />
-  );
+function Badge({ className, variant, ...props }: BadgeProps) {
+  return <div className={cn(badgeVariants({ variant }), className)} {...props} />;
 }
 
 export { Badge, badgeVariants };
